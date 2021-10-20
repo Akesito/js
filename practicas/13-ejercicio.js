22af8f57b66e5d9465c7ca67908040e8d4b919f5
Alexander <akexanderr123@gmail.com>
2021-10-20 13:32:08 -0500

    Day 3
    Day 3 of javascript for full stack development

---
+++ practicas/13-ejercicio.js
@@ -0,0 +1,11 @@
+/*
+Que me muestre los numeros pares de un rango de numero y los multiplique por 2 despues lo mismo pero dividido por 2
+*/
+
+for(let i =0;i<10;i++){
+    if(i%2===0){
+    console.log('los numeros pares son ' + i)
+    console.log('Estos numeros por 2 dan ' + i*2)
+    console.log('Estos numeros entre 2 dan' + i/2)
+    }
+}
---
+++ practicas/functions.js
@@ -0,0 +1,4 @@
+function operacionesBasicas(){
+    console.log(2*2)
+    console.log(2+2)
+}
